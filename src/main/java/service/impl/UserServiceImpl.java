8b2695a05edd2543518fcc75775c37766d6097e4
nvalfre <kokevalfre@gmail.com>
2023-03-08 23:43:19 -0300

    add validations, add password encoding

--- src/main/java/service/impl/UserServiceImpl.java
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -6,37 +6,71 @@ import lombok.RequiredArgsConstructor;
 import model.dto.UserDTO;
 import model.entity.Phone;
 import model.entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import repository.UserRepository;
 import service.UserService;
 
+import javax.persistence.EntityExistsException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    public static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*\\d{2,})(?=.*[a-z]).{8,12}$";
     private final UserRepository userRepository;
-    public User createUser(UserDTO userDto) {
+    private final PasswordEncoder passwordEncoder;
 
-        List<Phone> phones = userDto.getPhones().stream()
+    public User createUser(UserDTO userDto) {
+        if (!isValidEmail(userDto.getEmail())) {
+            throw new IllegalArgumentException("Invalid email address");
+        }
+        if (!isValidPassword(userDto.getPassword())) {
+            throw new IllegalArgumentException("Invalid password format");
+        }
+        if (userRepository.existsByEmail(userDto.getEmail())) {
+            throw new EntityExistsException("User with email already exists");
+        }
+
+        final List<Phone> phones = userDto.getPhones().stream()
                 .map(phoneDto -> new Phone(phoneDto.getNumber(), phoneDto.getCityCode(), phoneDto.getCountryCode()))
                 .collect(Collectors.toList());
+        final String token = generateToken(userDto);
+        final String encode = passwordEncoder.encode(userDto.getPassword());
 
-        String base64EncodedSecretKey = "";
-        String token = Jwts.builder()
-                .setSubject(userDto.getEmail())
-                .setIssuedAt(new Date())
-                .setExpiration(Date.from(LocalDateTime.now().plusMinutes(30L).atZone(ZoneId.systemDefault()).toInstant()))
+        return userRepository.save(buildNewUser(userDto, phones, token, encode));
+    }
+
+    private static User buildNewUser(UserDTO userDto, List<Phone> phones, String token, String encode) {
+        return new User(userDto.getName(), userDto.getEmail(), encode, LocalDateTime.now(), LocalDateTime.now(), token, true, phones);
+    }
+
+    private boolean isValidEmail(String email) {
+        final String regex = EMAIL_REGEX;
+        final Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(email).matches();
+    }
+
+    private boolean isValidPassword(String password) {
+        final String regex = PASSWORD_REGEX;
+        final Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(password).matches();
+    }
+
+    private String generateToken(UserDTO userDTO) {
+        final String base64EncodedSecretKey = "";
+        final LocalDateTime expirationTime = LocalDateTime.now().plus(30, ChronoUnit.MINUTES);
+        return Jwts.builder()
+                .setSubject(userDTO.getName())
+                .setExpiration(Date.from(expirationTime.atZone(ZoneId.systemDefault()).toInstant()))
                 .signWith(SignatureAlgorithm.HS256, base64EncodedSecretKey)
                 .compact();
-
-        User user = new User(userDto.getName(), userDto.getEmail(), userDto.getPassword(), LocalDateTime.now(), LocalDateTime.now(), token, true, phones);
-        userRepository.save(user);
-
-        return user;
     }
 }
